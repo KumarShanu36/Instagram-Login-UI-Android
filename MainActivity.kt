59d1b28cec1236f74ac7e0d42d43ae8afb61a342
KumarShanu36 <kumarshanu90848@gmail.com>
2025-11-26 15:27:10 +0530

    Add MainActivity with login functionality and to get popup for that "Login is Successful"

---
+++ MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun loginClicked(view: View) {
+        Toast.makeText(this, "Login is Successful", Toast.LENGTH_SHORT).show()
+    }
+}
